6b395bad077252e18165f1d80a8004fe95698080
Ezequiel França dos Santos <ezefranca@users.noreply.github.com>
2018-03-15 16:04:01 -0300

    Create solution.py

---
+++ day2/solution.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+import math
+
+if __name__ == "__main__":
+    meal_cost = float(input().strip())
+    tip_percent = int(input().strip())
+    tax_percent = int(input().strip())
+    print("The total meal cost is {} dollars.".format(math.floor(meal_cost + (tip_percent/100) * meal_cost + (tax_percent/100) * meal_cost)))
+
